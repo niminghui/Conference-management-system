ef0549132d086d47382466ed537de989f90fb7d6
Steve.Ni <gent_ni@126.com>
2018-11-14 16:03:25 +0800

    更新Account

--- src/main/java/scb/dev/sms/sm/dao/AccountDao.java
+++ src/main/java/scb/dev/sms/sm/dao/AccountDao.java
@@ -2,40 +2,69 @@ package scb.dev.sms.sm.dao;
 
 import scb.dev.sms.sm.pojo.Account;
 
+/**
+ * 
+ * ClassName: AccountDao <br/>
+ * Description: AccountDao. <br/>
+ *
+ * @author Minghui.Ni
+ * @version V1.0
+ * @since JDK 1.8 date: 2018年11月14日 下午3:21:36 <br/>
+ */
 public interface AccountDao {
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    int deleteByPrimaryKey(String accountId);
+	/**
+	 * Description:根据AccountID删除Account
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	int deleteByAccountId(String accountId);
 
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    int insert(Account record);
+	/**
+	 * Description:插入Account数据
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	int insert(Account record);
 
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    int insertSelective(Account record);
+	/**
+	 * Description:插入Account部分数据
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	int insertSelective(Account record);
 
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    Account selectByPrimaryKey(String accountId);
+	/**
+	 * Description:根据AccountID查找Account
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	Account selectByAccountId(String accountId);
 
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    int updateByPrimaryKeySelective(Account record);
+	/**
+	 * Description:根据AccountName查找Account
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	Account selectByAccountName(String accountName);
 
-    /**
-     *
-     * @mbg.generated 2018-11-14
-     */
-    int updateByPrimaryKey(Account record);
+	/**
+	 * Description:根据AccountName查找AccountID
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	String selectAccountIdByAccountName(String accountName);
+
+	/**
+	 * Description:选择性根据AccountID更新Account
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	int updateByAccountIdSelective(Account record);
+
+	/**
+	 * Description:根据AccountID更新Account
+	 * 
+	 * @mbg.generated 2018-11-14
+	 */
+	int updateByAccountId(Account record);
 }
--- src/main/java/scb/dev/sms/sm/pojo/Account.java
+++ src/main/java/scb/dev/sms/sm/pojo/Account.java
@@ -3,108 +3,162 @@ package scb.dev.sms.sm.pojo;
 import java.util.Date;
 
 public class Account {
-    /**
-     * 账户编号
-     */
-    private String accountId;
+	/**
+	 * 账户编号
+	 */
+	private String accountId;
 
-    /**
-     * 账户名称
-     */
-    private String accountName;
+	/**
+	 * 账户名称
+	 */
+	private String accountName;
 
-    /**
-     * 账户密码
-     */
-    private String accountPassword;
+	/**
+	 * 账户密码
+	 */
+	private String accountPassword;
 
-    /**
-     * 修改人名
-     */
-    private String accountUpdatedUser;
+	/**
+	 * 修改人名
+	 */
+	private String accountUpdatedUser;
 
-    /**
-     * 修改时间
-     */
-    private Date accountUpdatedTime;
+	/**
+	 * 修改时间
+	 */
+	private Date accountUpdatedTime;
 
-    /**
-     * 账户编号
-     * @return account_id 账户编号
-     */
-    public String getAccountId() {
-        return accountId;
-    }
+	/**
+	 * 账户编号
+	 * 
+	 * @return account_id 账户编号
+	 */
+	public String getAccountId() {
+		return accountId;
+	}
 
-    /**
-     * 账户编号
-     * @param accountId 账户编号
-     */
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
+	/**
+	 * 账户编号
+	 * 
+	 * @param accountId 账户编号
+	 */
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
 
-    /**
-     * 账户名称
-     * @return account_name 账户名称
-     */
-    public String getAccountName() {
-        return accountName;
-    }
+	/**
+	 * 账户名称
+	 * 
+	 * @return account_name 账户名称
+	 */
+	public String getAccountName() {
+		return accountName;
+	}
 
-    /**
-     * 账户名称
-     * @param accountName 账户名称
-     */
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
+	/**
+	 * 账户名称
+	 * 
+	 * @param accountName 账户名称
+	 */
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
 
-    /**
-     * 账户密码
-     * @return account_password 账户密码
-     */
-    public String getAccountPassword() {
-        return accountPassword;
-    }
+	/**
+	 * 账户密码
+	 * 
+	 * @return account_password 账户密码
+	 */
+	public String getAccountPassword() {
+		return accountPassword;
+	}
 
-    /**
-     * 账户密码
-     * @param accountPassword 账户密码
-     */
-    public void setAccountPassword(String accountPassword) {
-        this.accountPassword = accountPassword;
-    }
+	/**
+	 * 账户密码
+	 * 
+	 * @param accountPassword 账户密码
+	 */
+	public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+	}
 
-    /**
-     * 修改人名
-     * @return account_updated_user 修改人名
-     */
-    public String getAccountUpdatedUser() {
-        return accountUpdatedUser;
-    }
+	/**
+	 * 修改人名
+	 * 
+	 * @return account_updated_user 修改人名
+	 */
+	public String getAccountUpdatedUser() {
+		return accountUpdatedUser;
+	}
 
-    /**
-     * 修改人名
-     * @param accountUpdatedUser 修改人名
-     */
-    public void setAccountUpdatedUser(String accountUpdatedUser) {
-        this.accountUpdatedUser = accountUpdatedUser;
-    }
+	/**
+	 * 修改人名
+	 * 
+	 * @param accountUpdatedUser 修改人名
+	 */
+	public void setAccountUpdatedUser(String accountUpdatedUser) {
+		this.accountUpdatedUser = accountUpdatedUser;
+	}
 
-    /**
-     * 修改时间
-     * @return account_updated_time 修改时间
-     */
-    public Date getAccountUpdatedTime() {
-        return accountUpdatedTime;
-    }
+	/**
+	 * 修改时间
+	 * 
+	 * @return account_updated_time 修改时间
+	 */
+	public Date getAccountUpdatedTime() {
+		return accountUpdatedTime;
+	}
 
-    /**
-     * 修改时间
-     * @param accountUpdatedTime 修改时间
-     */
-    public void setAccountUpdatedTime(Date accountUpdatedTime) {
-        this.accountUpdatedTime = accountUpdatedTime;
-    }
+	/**
+	 * 修改时间
+	 * 
+	 * @param accountUpdatedTime 修改时间
+	 */
+	public void setAccountUpdatedTime(Date accountUpdatedTime) {
+		this.accountUpdatedTime = accountUpdatedTime;
+	}
+
+	/**
+	 * 
+	 * Creates a new instance of Account.
+	 *
+	 */
+	public Account() {
+		super();
+	}
+
+	/**
+	 * Creates a new instance of Account.
+	 *
+	 * @param accountId
+	 * @param accountName
+	 * @param accountPassword
+	 */
+	public Account(String accountId, String accountName, String accountPassword) {
+		super();
+		this.accountId = accountId;
+		this.accountName = accountName;
+		this.accountPassword = accountPassword;
+	}
+
+	/**
+	 * Creates a new instance of Account.
+	 *
+	 * @param accountId
+	 * @param accountName
+	 * @param accountPassword
+	 * @param accountUpdatedUser
+	 * @param accountUpdatedTime
+	 */
+
+	public Account(String accountId, String accountName, String accountPassword, String accountUpdatedUser,
+			Date accountUpdatedTime) {
+		super();
+		this.accountId = accountId;
+		this.accountName = accountName;
+		this.accountPassword = accountPassword;
+		this.accountUpdatedUser = accountUpdatedUser;
+		this.accountUpdatedTime = accountUpdatedTime;
+	}
+
 }
