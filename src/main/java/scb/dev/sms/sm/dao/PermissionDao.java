f098a2b10f91d4a1f92b7548dceabb65c23659cc
Menciusbj <3031133416@qq.com>
2018-11-20 14:59:54 +0800

    更新了permission 和rolegroup Dao pojo xml

---
+++ src/main/java/scb/dev/sms/sm/dao/PermissionDao.java
@@ -0,0 +1,41 @@
+package scb.dev.sms.sm.dao;
+
+import scb.dev.sms.sm.pojo.Permission;
+
+public interface PermissionDao {
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	int deleteByPrimaryKey(String permissionId);
+
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	int insert(Permission record);
+
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	int insertSelective(Permission record);
+
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	Permission selectByPrimaryKey(String permissionId);
+
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	int updateByPrimaryKeySelective(Permission record);
+
+	/**
+	 *
+	 * @mbg.generated 2018-11-20
+	 */
+	int updateByPrimaryKey(Permission record);
+}
---
+++ src/main/java/scb/dev/sms/sm/pojo/Permission.java
@@ -0,0 +1,49 @@
+package scb.dev.sms.sm.pojo;
+
+public class Permission {
+	/**
+	 * 功能编号
+	 */
+	private String permissionId;
+
+	/**
+	 * 功能名称
+	 */
+	private String permissionName;
+
+	/**
+	 * 功能编号
+	 * 
+	 * @return permission_id 功能编号
+	 */
+	public String getPermissionId() {
+		return permissionId;
+	}
+
+	/**
+	 * 功能编号
+	 * 
+	 * @param permissionId 功能编号
+	 */
+	public void setPermissionId(String permissionId) {
+		this.permissionId = permissionId;
+	}
+
+	/**
+	 * 功能名称
+	 * 
+	 * @return permission_name 功能名称
+	 */
+	public String getPermissionName() {
+		return permissionName;
+	}
+
+	/**
+	 * 功能名称
+	 * 
+	 * @param permissionName 功能名称
+	 */
+	public void setPermissionName(String permissionName) {
+		this.permissionName = permissionName;
+	}
+}
