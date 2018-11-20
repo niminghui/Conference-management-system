2ae6dcf97642cef96b2c533f106834d4ecbd5c9c
Menciusbj <3031133416@qq.com>
2018-11-20 15:50:36 +0800

    permission 和rolegroup的pojo Dao Xml

--- src/main/java/scb/dev/sms/sm/dao/RoleGroupDao.java
+++ src/main/java/scb/dev/sms/sm/dao/RoleGroupDao.java
@@ -3,39 +3,39 @@ package scb.dev.sms.sm.dao;
 import scb.dev.sms.sm.pojo.RoleGroup;
 
 public interface RoleGroupDao {
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	int deleteByPrimaryKey(String rolegroupId);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    int deleteByPrimaryKey(String rolegroupId);
 
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	int insert(RoleGroup record);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    int insert(RoleGroup record);
 
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	int insertSelective(RoleGroup record);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    int insertSelective(RoleGroup record);
 
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	RoleGroup selectByPrimaryKey(String rolegroupId);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    RoleGroup selectByPrimaryKey(String rolegroupId);
 
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	int updateByPrimaryKeySelective(RoleGroup record);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    int updateByPrimaryKeySelective(RoleGroup record);
 
-	/**
-	 *
-	 * @mbg.generated 2018-11-20
-	 */
-	int updateByPrimaryKey(RoleGroup record);
+    /**
+     *
+     * @mbg.generated 2018-11-20
+     */
+    int updateByPrimaryKey(RoleGroup record);
 }
--- src/main/java/scb/dev/sms/sm/pojo/RoleGroup.java
+++ src/main/java/scb/dev/sms/sm/pojo/RoleGroup.java
@@ -3,164 +3,150 @@ package scb.dev.sms.sm.pojo;
 import java.util.Date;
 
 public class RoleGroup {
-	/**
-	 * 角色组编号
-	 */
-	private String rolegroupId;
+    /**
+     * 角色组编号
+     */
+    private String rolegroupId;
 
-	/**
-	 * 角色组名称
-	 */
-	private String rolegroupName;
+    /**
+     * 角色组名称
+     */
+    private String rolegroupName;
 
-	/**
-	 * 角色组描述
-	 */
-	private String rolegroupDescribed;
+    /**
+     * 角色组描述
+     */
+    private String rolegroupDescribed;
 
-	/**
-	 * 修改人名
-	 */
-	private String rolegroupUpdatedUser;
+    /**
+     * 修改人名
+     */
+    private String rolegroupUpdatedUser;
 
-	/**
-	 * 修改时间
-	 */
-	private Date rolegroupUpdatedTime;
+    /**
+     * 修改时间
+     */
+    private Date rolegroupUpdatedTime;
 
-	/**
-	 * 创建人名
-	 */
-	private String rolegroupCreatedUser;
+    /**
+     * 创建人名
+     */
+    private String rolegroupCreatedUser;
 
-	/**
-	 * 创建时间
-	 */
-	private Date rolegroupCreatedTime;
+    /**
+     * 创建时间
+     */
+    private Date rolegroupCreatedTime;
 
-	/**
-	 * 角色组编号
-	 * 
-	 * @return rolegroup_id 角色组编号
-	 */
-	public String getRolegroupId() {
-		return rolegroupId;
-	}
+    /**
+     * 角色组编号
+     * @return rolegroup_id 角色组编号
+     */
+    public String getRolegroupId() {
+        return rolegroupId;
+    }
 
-	/**
-	 * 角色组编号
-	 * 
-	 * @param rolegroupId 角色组编号
-	 */
-	public void setRolegroupId(String rolegroupId) {
-		this.rolegroupId = rolegroupId;
-	}
+    /**
+     * 角色组编号
+     * @param rolegroupId 角色组编号
+     */
+    public void setRolegroupId(String rolegroupId) {
+        this.rolegroupId = rolegroupId;
+    }
 
-	/**
-	 * 角色组名称
-	 * 
-	 * @return rolegroup_name 角色组名称
-	 */
-	public String getRolegroupName() {
-		return rolegroupName;
-	}
+    /**
+     * 角色组名称
+     * @return rolegroup_name 角色组名称
+     */
+    public String getRolegroupName() {
+        return rolegroupName;
+    }
 
-	/**
-	 * 角色组名称
-	 * 
-	 * @param rolegroupName 角色组名称
-	 */
-	public void setRolegroupName(String rolegroupName) {
-		this.rolegroupName = rolegroupName;
-	}
+    /**
+     * 角色组名称
+     * @param rolegroupName 角色组名称
+     */
+    public void setRolegroupName(String rolegroupName) {
+        this.rolegroupName = rolegroupName;
+    }
 
-	/**
-	 * 角色组描述
-	 * 
-	 * @return rolegroup_described 角色组描述
-	 */
-	public String getRolegroupDescribed() {
-		return rolegroupDescribed;
-	}
+    /**
+     * 角色组描述
+     * @return rolegroup_described 角色组描述
+     */
+    public String getRolegroupDescribed() {
+        return rolegroupDescribed;
+    }
 
-	/**
-	 * 角色组描述
-	 * 
-	 * @param rolegroupDescribed 角色组描述
-	 */
-	public void setRolegroupDescribed(String rolegroupDescribed) {
-		this.rolegroupDescribed = rolegroupDescribed;
-	}
+    /**
+     * 角色组描述
+     * @param rolegroupDescribed 角色组描述
+     */
+    public void setRolegroupDescribed(String rolegroupDescribed) {
+        this.rolegroupDescribed = rolegroupDescribed;
+    }
 
-	/**
-	 * 修改人名
-	 * 
-	 * @return rolegroup_updated_user 修改人名
-	 */
-	public String getRolegroupUpdatedUser() {
-		return rolegroupUpdatedUser;
-	}
+    /**
+     * 修改人名
+     * @return rolegroup_updated_user 修改人名
+     */
+    public String getRolegroupUpdatedUser() {
+        return rolegroupUpdatedUser;
+    }
 
-	/**
-	 * 修改人名
-	 * 
-	 * @param rolegroupUpdatedUser 修改人名
-	 */
-	public void setRolegroupUpdatedUser(String rolegroupUpdatedUser) {
-		this.rolegroupUpdatedUser = rolegroupUpdatedUser;
-	}
+    /**
+     * 修改人名
+     * @param rolegroupUpdatedUser 修改人名
+     */
+    public void setRolegroupUpdatedUser(String rolegroupUpdatedUser) {
+        this.rolegroupUpdatedUser = rolegroupUpdatedUser;
+    }
 
-	/**
-	 * 修改时间
-	 * 
-	 * @return rolegroup_updated_time 修改时间
-	 */
-	public Date getRolegroupUpdatedTime() {
-		return rolegroupUpdatedTime;
-	}
+    /**
+     * 修改时间
+     * @return rolegroup_updated_time 修改时间
+     */
+    public Date getRolegroupUpdatedTime() {
+        return rolegroupUpdatedTime;
+    }
 
-	/**
-	 * 修改时间
-	 * 
-	 * @param rolegroupUpdatedTime 修改时间
-	 */
-	public void setRolegroupUpdatedTime(Date rolegroupUpdatedTime) {
-		this.rolegroupUpdatedTime = rolegroupUpdatedTime;
-	}
+    /**
+     * 修改时间
+     * @param rolegroupUpdatedTime 修改时间
+     */
+    public void setRolegroupUpdatedTime(Date rolegroupUpdatedTime) {
+        this.rolegroupUpdatedTime = rolegroupUpdatedTime;
+    }
 
-	/**
-	 * 创建人名
-	 * 
-	 * @return rolegroup_created_user 创建人名
-	 */
-	public String getRolegroupCreatedUser() {
-		return rolegroupCreatedUser;
-	}
+    /**
+     * 创建人名
+     * @return rolegroup_created_user 创建人名
+     */
+    public String getRolegroupCreatedUser() {
+        return rolegroupCreatedUser;
+    }
 
-	/**
-	 * 创建人名
-	 * 
-	 * @param rolegroupCreatedUser 创建人名
-	 */
-	public void setRolegroupCreatedUser(String rolegroupCreatedUser) {
-		this.rolegroupCreatedUser = rolegroupCreatedUser;
-	}
+    /**
+     * 创建人名
+     * @param rolegroupCreatedUser 创建人名
+     */
+    public void setRolegroupCreatedUser(String rolegroupCreatedUser) {
+        this.rolegroupCreatedUser = rolegroupCreatedUser;
+    }
 
-	/**
-	 * 创建时间
-	 * 
-	 * @return rolegroup_created_time 创建时间
-	 */
-	public Date getRolegroupCreatedTime() {
-		return rolegroupCreatedTime;
-	}
+    /**
+     * 创建时间
+     * @return rolegroup_created_time 创建时间
+     */
+    public Date getRolegroupCreatedTime() {
+        return rolegroupCreatedTime;
+    }
 
-	/**
-	 * 创建时间
-	 * 
-	 * @param rolegroupCreatedTime 创建时间
-	 */
-	public void setRolegroupCreatedTime(Date rolegroupCreatedTime) {
-		this.rolegroupCreatedTime = rolegroupCreatedTime;
-	}
+    /**
+     * 创建时间
+     * @param rolegroupCreatedTime 创建时间
+     */
+    public void setRolegroupCreatedTime(Date rolegroupCreatedTime) {
+        this.rolegroupCreatedTime = rolegroupCreatedTime;
+    }
 }
