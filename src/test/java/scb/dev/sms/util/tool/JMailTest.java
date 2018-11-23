c6fb498c58cee7a04e35c4c99668f61a46d0b27d
Steve.Ni <gent_ni@126.com>
2018-11-23 13:25:01 +0800

    code format

--- src/test/java/scb/dev/sms/util/tool/JMailTest.java
+++ src/test/java/scb/dev/sms/util/tool/JMailTest.java
@@ -10,7 +10,7 @@ package scb.dev.sms.util.tool;
 
 /**
  * ClassName: JMailTest <br/>
- * Description: TODO ADD REASON(可选). <br/><br/>
+ * Description: JMailTest <br/>
  * date: 2018年11月23日 上午10:08:41 <br/>
  *
  * @author Zither.Chen
@@ -18,12 +18,9 @@ package scb.dev.sms.util.tool;
  * @since JDK 1.8
  */
 public class JMailTest {
-	//测试方法
-		public static void main(String[] args) throws Exception {
-	    	
-	        JMail.sendMail("zither_chen@163.com", "zither_chen", "CHENKE517",
-	                "1070726094@qq.com",
-	                "Java Mail 测试邮件",
-	                "123");
-	    }
+	// 测试方法
+	public static void main(String[] args) throws Exception {
+
+		JMail.sendMail("zither_chen@163.com", "zither_chen", "CHENKE517", "1070726094@qq.com", "Java Mail 测试邮件", "123");
+	}
 }
